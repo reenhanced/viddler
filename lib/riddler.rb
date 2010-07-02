2bea8f4f0b2935d857e72894a1a0a81c5ccdfda5
Kyle Slattery <k@kslat.com>
2010-07-02 16:23:58 -0400

    Start fresh

--- lib/riddler.rb
+++ lib/riddler.rb
@@ -3,14 +3,3 @@ require 'bundler'
 Bundler.setup
 
 require 'rest_client'
-
-require 'riddler/client'
-
-module Riddler
-  V2_METHODS = [
-    'viddler.api.getInfo',
-    'viddler.users.auth',
-    'viddler.users.getProfile',
-    'viddler.users.getFriends'
-  ]
-end
