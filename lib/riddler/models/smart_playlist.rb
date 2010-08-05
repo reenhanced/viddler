16456a1e910664afe3e084947773f585a90fc70b
Kyle Slattery <k@kslat.com>
2010-08-05 11:30:39 -0400

    Spec some SmartPlaylist specifics

--- lib/riddler/models/smart_playlist.rb
+++ lib/riddler/models/smart_playlist.rb
@@ -1,4 +1,17 @@
 module Riddler
   class SmartPlaylist < Playlist
+    attr_reader :rules
+    
+    def self.create!(session, attributes)
+      attributes[:type] = "smart"
+      super
+    end
+    
+    protected
+    
+    def populate_from_response!(response)
+      @rules = response['list_result']['playlist']['rules'] if response['list_result'] && response['list_result']['playlist']
+      super
+    end
   end
 end
