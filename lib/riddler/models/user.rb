b1de0ad3fe835c340f01a7d42a19d2e6bf3ec8b9
Kyle Slattery <k@kslat.com>
2010-08-11 16:24:57 -0400

    Add Riddler::User

---
+++ lib/riddler/models/user.rb
@@ -0,0 +1,21 @@
+module Riddler
+  class User
+    STRING_ATTRIBUTES = [:username, :first_name, :last_name, :about_me,
+                         :avatar, :homepage, :company, :gender, :city]
+                        
+    INT_ATTRIBUTES    = [:age, :video_upload_count, :video_watch_count,
+                         :friend_count, :favourite_video_count]
+
+    (STRING_ATTRIBUTES + INT_ATTRIBUTES).each {|a| attr_accessor a}
+    
+    def initialize(session, response)
+      STRING_ATTRIBUTES.each do |attr|
+        instance_variable_set("@#{attr.to_s}", response['user'][attr.to_s]) if response['user'][attr.to_s]
+      end
+      
+      INT_ATTRIBUTES.each do |attr|
+        instance_variable_set("@#{attr.to_s}", response['user'][attr.to_s].to_i) if response['user'][attr.to_s]
+      end
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,89 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Riddler::User, ".new" do
+  before(:each) do
+    @response = {
+      "user" => {
+        "username" => "kyleslat",
+        "first_name" => "Kyle",
+        "last_name" => "Slattery",
+        "about_me" => "Some about me",
+        "avatar" => "http://myavatar.com/image.jpg",
+        "age" => "99",
+        "video_upload_count" => "50",
+        "video_watch_count" => "111",
+        "homepage" => "http://www.viddler.com",
+        "gender" => "m",
+        "company" => "Viddler",
+        "city" => "Bethlehem, PA",
+        "friend_count" => "200",
+        "favourite_video_count" => "1999"
+      }
+    }
+    
+    @client  = mock(Riddler::Client, :get => @response)
+    @session = mock(Riddler::Session, :client => @client)
+    
+    @user = Riddler::User.new(@session, @response)
+  end
+  
+  it "requires session and response" do
+    lambda {Riddler::User.new}.should raise_error(ArgumentError, /0 for 2/)
+  end
+  
+  it "sets username" do
+    @user.username.should == "kyleslat"
+  end
+  
+  it "sets first_name" do
+    @user.first_name.should == "Kyle"
+  end
+  
+  it "sets last_name" do
+    @user.last_name.should == "Slattery"
+  end
+  
+  it "sets about_me" do
+    @user.about_me.should == "Some about me"
+  end
+  
+  it "sets avatar" do
+    @user.avatar.should == "http://myavatar.com/image.jpg"
+  end
+  
+  it "sets age" do
+    @user.age.should == 99
+  end
+  
+  it "sets video_upload_count" do
+    @user.video_upload_count.should == 50
+  end
+  
+  it "sets video_watch_count" do
+    @user.video_watch_count.should == 111
+  end
+  
+  it "sets homepage" do
+    @user.homepage.should == "http://www.viddler.com"
+  end
+  
+  it "sets gender" do
+    @user.gender.should == "m"
+  end
+  
+  it "sets company" do
+    @user.company.should == "Viddler"
+  end
+  
+  it "sets city" do
+    @user.city.should == "Bethlehem, PA"
+  end
+  
+  it "sets friend_count" do
+    @user.friend_count.should == 200
+  end
+  
+  it "sets favourite_video_count" do
+    @user.favourite_video_count.should == 1999
+  end
+end
