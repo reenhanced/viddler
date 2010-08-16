4a36197427b6fbf6c7d2dbf33943884f0a53d9ed
Kyle Slattery <k@kslat.com>
2010-08-16 13:56:59 -0400

    Revert "Fix list_name for video lists"
    This reverts commit a252750bf7cf6f47ec4e8bcef6e2c8a9007c0165.

--- lib/riddler/models/video_list.rb
+++ lib/riddler/models/video_list.rb
@@ -2,7 +2,7 @@ module Riddler
   class VideoList < Array
     attr_reader :page, :per_page
     
-    def initialize(session, response, list_name="video_list")
+    def initialize(session, response, list_name="videos_list")
       @page     = response["list_result"]["page"].to_i
       @per_page = response["list_result"]["per_page"].to_i
       
