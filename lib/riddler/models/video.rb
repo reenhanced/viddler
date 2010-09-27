f7077d77932acb9a65912d9a4ba89b947d425bdf
Robert Stern <robert@reenhanced.com>
2010-09-27 11:40:42 -0400

    Added embed_code to video's attributes.

--- lib/riddler/models/video.rb
+++ lib/riddler/models/video.rb
@@ -6,7 +6,7 @@ module Riddler
     
     attr_reader :id, :status, :author, :title, :length, :description, :url,
                 :thumbnail_url, :permalink, :html5_video_source, :view_count,
-                :comment_count, :uploaded_at, :made_public_at
+                :comment_count, :uploaded_at, :made_public_at, :embed_code
     
     def initialize(session, response)
       @id                 = response["id"]
@@ -24,6 +24,7 @@ module Riddler
       @favorited          = response["favorite"].to_i
       @uploaded_at        = Time.at(response["upload_time"].to_i)
       @made_public_at     = Time.at(response["made_public_time"].to_i)
+      @embed_code         = response["embed_code"]
     end
     
     def self.find_by_username(session, username, options={})
@@ -47,6 +48,10 @@ module Riddler
         url.gsub!(/_[0-9]_/, "_#{THUMBNAIL_SIZES.rindex(size)}_")
       end
       url
+    end
+    
+    def embed_code
+      @embed_code || raise Riddler::Exceptions::ApiError.new(4, "Embed code not included in response.", "Pass :add_embed_code => true in arguments to return a video's embed_code.")
     end
   end
 end
