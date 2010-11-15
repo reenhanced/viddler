caeb3b3e0dc6c8cf3705dd8edc4f3cf44347fb29
Kyle Slattery <k@kslat.com>
2010-11-14 20:29:22 -0500

    Modify VideoList#each and VideoList#[]

--- lib/riddler/models/video_list.rb
+++ lib/riddler/models/video_list.rb
@@ -9,6 +9,54 @@ module Riddler
       
       return if response.empty?
       
+      insert_response(response)
+    end
+    
+    def each
+      i = 0
+      keep_going = true
+      
+      while keep_going
+        fetch_page(i/100+1) if self[i].nil?
+        
+        if self[i].nil?
+          keep_going = false
+        else
+          yield self[i]
+          i += 1
+        end
+      end
+    end
+    
+    def [](start, length=1)
+      if start.is_a?(Range)
+        length = start.end - start.begin + 1
+        start  = start.begin
+      end
+      
+      i   = start
+      arr = []
+      
+      length.times do
+        fetch_page(i/100+1) if super(i).nil?
+        arr << super(i) unless super(i).nil?
+        i += 1
+      end
+      
+      if length == 1
+        return arr[0]
+      else
+        return arr
+      end
+    end
+    
+    private
+    
+    def fetch_page(page)
+      insert_response(session.client.get(list_method, list_method_options.merge(:page => page, :per_page => 100)))
+    end
+    
+    def insert_response(response)
       page     = response["list_result"]["page"].to_i
       per_page = response["list_result"]["per_page"].to_i
       
