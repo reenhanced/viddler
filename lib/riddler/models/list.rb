34c0317c1da168119b7915eb746aaae426c37703
Kyle Slattery <k@kslat.com>
2011-01-05 14:23:52 -0500

    Remove overly fancy List implementation--now it only returns what you're asking for

--- lib/riddler/models/list.rb
+++ lib/riddler/models/list.rb
@@ -9,58 +9,8 @@ module Riddler
       
       insert_response(response) unless response.empty?
     end
-    
-    def page(page, options={})
-      options = {:per_page => 10}.merge(options)
-      offset  = options[:per_page]*(page-1)
-      self[offset, options[:per_page]]
-    end
-    
-    def all
-      i = 0
-      keep_going = true
-      
-      while keep_going
-        if self[i].nil?
-          keep_going = false
-        else
-          yield self[i]
-          i += 1
-        end
-      end
-    end
-    
-    def [](start, length=1)
-      if start.is_a?(Range)
-        length = start.end - start.begin + 1
-        start  = start.begin
-      end
-      
-      i   = start
-      arr = []
 
-      length.times do
-        fetch_page(i/100+1) if super(i).nil?
-        break if super(i).nil?
-        
-        arr << super(i) 
-        i += 1
-      end
-      
-      if length == 1
-        return arr[0]
-      else
-        return arr
-      end
-    end
-    
-    alias_method :slice, :[]
-    
     private
-    
-    def fetch_page(page)
-      insert_response(session.client.get(list_method, list_method_options.merge(:page => page, :per_page => 100)))
-    end
     
     def insert_response(response)
       page     = response["list_result"]["page"].to_i
